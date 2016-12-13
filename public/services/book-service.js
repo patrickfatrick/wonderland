0d4b9546ed792711b3b119bb51ac697e68894c5a
Patrick Fricano <patrick.fricano@icloud.com>
2016-12-13 08:20:06 -0700

    remove unnecessary function

--- public/services/book-service.js
+++ public/services/book-service.js
@@ -1,17 +1,5 @@
 /* globals fetch */
 
-export function getCaptions (cb) {
-  return fetch('./data/data.json')
-  .then((response) => response.json())
-  .then((response) => {
-    const captions = response.captions.map((caption, i) => {
-      if (i === 0) return { ...caption, active: true }
-      return { ...caption, active: false }
-    })
-    cb(captions)
-  })
-}
-
 export function getBook (location, cb) {
   return fetch(location)
   .then((response) => response.json())
