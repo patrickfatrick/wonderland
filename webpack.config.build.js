0c94d177fc5a69794de5b83c17ad0858a2f818bc
Patrick Fricano <patrick.fricano@icloud.com>
2019-02-19 13:32:05 -0800

    use terser plugin

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,20 +1,11 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const merge = require('webpack-merge');
 const base = require('./webpack.config.base');
 
 module.exports = merge(base, {
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        sourceMap: false,
-        uglifyOptions: {
-          output: { comments: false },
-          compress: {
-            dead_code: true,
-            drop_console: true,
-          },
-        },
-      }),
+      new TerserPlugin(),
     ],
   },
 });
